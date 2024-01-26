301a9e71c2c58d7e147a99d4a13a272b1b563115
Tai Nguyen <tai.nguyen5101@yahoo.com>
2024-01-25 20:03:48 -0800

    Voice acting tab

--- martingale2.py
+++ martingale2.py
@@ -5,20 +5,23 @@ import numpy as np
 def winning_probability(dollars_risked, dollars_gained):
     return (1 - 0.6**(math.log2(dollars_risked)+1))**dollars_gained
 
-def chance_of_winning():
+def chance_of_winning(money_risking=128):
     x = np.linspace(0,100)
-    y = winning_probability(128,x)
-    plt.plot(x,y)
+    y = winning_probability(money_risking,x)
+    plt.plot(x,y,label="Martingale risk curve")
     plt.xlabel("Money gained")
     plt.ylabel("Probability of \"succeeding\"")
-    plt.title("Chance you will win for a money gained")
-    plt.show()
+    plt.title(f"Chance you will win for a money gained while risking {money_risking}")
+    plt.legend()
+    
 
-def regular_log(x):
-    return x**-0.9
+def exponential(x):
+    return math.exp(x*-0.01)
 
 if __name__ == "__main__":
-    x = np.linspace(1,100)
-    func = np.vectorize(regular_log)
-    plt.plot(x,func(x))
-    chance_of_winning()
+    x = np.linspace(0,100)
+    func = np.vectorize(exponential)
+    plt.plot(x,func(x),label="My risk curve")
+    chance_of_winning(128)
+    plt.legend()
+    plt.show()
